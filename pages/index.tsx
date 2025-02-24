69b4974cdbfcccae39efc3f4908a88bb2d501eab
abhay <abhay.singh@tothenew.comgit config --global user.name abhaygit config --global user.email abhay.singh@tothenew.com>
2025-02-24 17:14:47 +0530

    fix:home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,46 @@
 "use client";
-import React from "react";
+import React, { use } from "react";
 import { useEffect,useState } from "react";
-
+import { useRouter } from "next/navigation";
+interface ProductTypes{
+  id:number;
+  title:string;
+  description:string;
+}
 export default function Product() {
+  const router = useRouter();
 
-  const [products,setProducts] = useState();
+  const [products,setProducts] = useState<ProductTypes[]>([]);
   useEffect(()=>{
     const fetchProducts = async()=>{
       const result = await fetch("https://dummyjson.com/products");
       const data = await result.json();
-      console.log(data.products);
-      setProducts(data.products);
+
+      setProducts(data?.products);
+    
     }
 
 
-    console.log("products-->",products);
+    // console.log("products-->",products);
 
     fetchProducts();
-  },[products]);
+  },[]);
+
+  const handleButton=(id:number)=>{
+    router.push(`/product/${id}`);
+  }
 
   return (
-    <div>
-      <h1>PRODUCTS</h1>
+    <div className="flex flex-col gap-4 p-4">
+      <h1 className="text-2xl font-bold">PRODUCTS</h1>
+      {products.map((product)=>(
+        <div key={product.id} className="border border-gray-300 rounded-md p-4">
+          <h2><strong>TITLE: </strong>{product.title}</h2>
+          <p><strong>DESCRIPTION: </strong>{product.description}</p>
+
+            <button onClick={()=>handleButton(product.id)} className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">VIEW DETAILS</button>
+        </div>
+      ))}
       
     </div>
   );
